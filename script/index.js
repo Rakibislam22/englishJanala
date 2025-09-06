21b7eb38a21d55b53c714bed695e3e0d4892930e
Rakibislam22 <rakibislam13122018@gmail.com>
2025-09-06 12:57:13 +0600

    Bug fix

--- script/index.js
+++ script/index.js
@@ -1,8 +1,8 @@
 // featch lesson label
-const loadData = ()=>{
+const loadData = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all')
-    .then(response => response.json())
-    .then(data => label(data.data));
+        .then(response => response.json())
+        .then(data => label(data.data));
 
 }
 
@@ -10,29 +10,29 @@ const loadData = ()=>{
 const selectLesson = (id) => {
     spinner(true);
     fetch(`https://openapi.programming-hero.com/api/level/${id}`)
-    .then(response => response.json())
-    .then(data =>{
-        document.getElementById('input-c').value = '';
-        const previousActive = document.querySelectorAll('.active');
-        previousActive.forEach(element => {
-            element.classList.remove('active');
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('input-c').value = '';
+            const previousActive = document.querySelectorAll('.active');
+            previousActive.forEach(element => {
+                element.classList.remove('active');
+            });
+
+            document.getElementById(`lesson-${id}`).classList.add('active');
+
+            displayWord(data.data);
+            spinner(false);
         });
 
-        document.getElementById(`lesson-${id}`).classList.add('active');
-
-        displayWord(data.data) ;
-        spinner(false);
-    });
-    
 }
 
 // spninner
-const spinner = (show) =>{
-    if(show){
+const spinner = (show) => {
+    if (show) {
         document.getElementById('loading').classList.remove('hidden');
         document.getElementById('word-container').classList.add('hidden');
     }
-    else{
+    else {
         document.getElementById('loading').classList.add('hidden');
         document.getElementById('word-container').classList.remove('hidden');
     }
@@ -40,12 +40,12 @@ const spinner = (show) =>{
 }
 
 // spninner for modal
-const modalSpinner = (show) =>{
-    if(show){
+const modalSpinner = (show) => {
+    if (show) {
         document.getElementById('modal-l').classList.remove('hidden');
         document.getElementById('modal-content').classList.add('hidden');
     }
-    else{
+    else {
         document.getElementById('modal-l').classList.add('hidden');
         document.getElementById('modal-content').classList.remove('hidden');
     }
@@ -53,53 +53,82 @@ const modalSpinner = (show) =>{
 
 // pronounce word
 function pronounceWord(word) {
-  const utterance = new SpeechSynthesisUtterance(word);
-  utterance.lang = "en-EN"; // English
-  window.speechSynthesis.speak(utterance);
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.lang = "en-EN"; // English
+    window.speechSynthesis.speak(utterance);
 }
 
 // featch all word info
 const allword = () => {
     spinner(true);
     fetch(`https://openapi.programming-hero.com/api/words/all`)
-    .then(response => response.json())
-    .then(data => {
-        const inputch = document.getElementById('input-c').value.trim().toLowerCase();
-        const matchedW = data.data.filter(element => element.word.toLowerCase().includes(inputch));
-        
-        const previousActive = document.querySelectorAll('.active');
-        previousActive.forEach(element => {
-            element.classList.remove('active');
+        .then(response => response.json())
+        .then(data => {
+            const inputch = document.getElementById('input-c').value.trim().toLowerCase();
+            const matchedW = data.data.filter(element => element.word.toLowerCase().includes(inputch));
+
+            const previousActive = document.querySelectorAll('.active');
+            previousActive.forEach(element => {
+                element.classList.remove('active');
+            });
+
+            const container = document.getElementById('word-container');
+            if (matchedW.length === 0) {
+
+                container.innerHTML = '';
+                container.innerHTML = `
+   
+                <div class="py-8 space-y-4 text-center col-span-full">
+                    <img src="assets/alert-error.png" alt="" class="mx-auto ">
+                    <p class="bangla text-[#79716B]">এই Vocabulary এখনো যুক্ত করা হয়নি।।</p>
+                    <h1 class="text-3xl text-[#292524] font-medium bangla"> অন্য Vocabulary Search করুন</h1>
+                </div>
+            
+                `;
+            }
+            else if (inputch.length === 0) {
+
+                container.innerHTML = '';
+                container.innerHTML = `
+   
+                <div class="py-8 space-y-4 text-center col-span-full">
+                    <img src="assets/alert-error.png" alt="" class="mx-auto ">
+                    <p class="bangla text-[#79716B]">কোন Vocabulary লেখেননি।।</p>
+                    <h1 class="text-3xl text-[#292524] font-medium bangla">Vocabulary লেখে Search করুন</h1>
+                </div>
+            
+                `;
+            }
+            else {
+                displayWord(matchedW);
+            }
+            spinner(false);
+
         });
-
-        displayWord(matchedW);
-        spinner(false);
-
-    });
 }
 
 // load word info in modal
-const loadWordInfo = (id) =>{
+const loadWordInfo = (id) => {
     modalSpinner(true);
     my_modal_5.showModal();
     fetch(`https://openapi.programming-hero.com/api/word/${id}`)
-    .then(response => response.json())
-    .then(data => {
-        const modal = document.getElementById('modal-content');
-        modal.innerHTML = `
+        .then(response => response.json())
+        .then(data => {
+            const modal = document.getElementById('modal-content');
+            modal.innerHTML = `
             <div>
-                <h1 class="text-2xl font-bold">${data.data.word}  (<i class="fa-solid fa-microphone-lines"></i>: ${data.data.pronunciation})</h1>
+                <h1 class="text-2xl font-bold">${(data.data.word)? data.data.word : 'শব্দ পাওয়া যায়নি'}  (<i class="fa-solid fa-microphone-lines"></i>: ${(data.data.pronunciation)? data.data.pronunciation : 'উচ্চারণ পাওয়া যায়নি'})</h1>
             </div>
             <div class="pt-4">
                 <h3 class="py-1 font-bold">Meaning  </h3>
-                <p class="py-1">${data.data.meaning}</p>
+                <p class="py-1">${(data.data.meaning) ? data.data.meaning : 'অর্থ পাওয়া যায়নি'}</p>
             </div>
             <div>
             
             </div>
             <div class="pt-4">
                 <h3 class="py-1 font-bold">Example</h3> 
-                <p class="py-1">${data.data.sentence}</p>
+                <p class="py-1">${(data.data.sentence) ? data.data.sentence : 'উদাহরণ পাওয়া যায়নি'}</p>
             </div>
 
             <div class="pt-4">
@@ -110,18 +139,18 @@ const loadWordInfo = (id) =>{
             </div>          
             
         `;
-        my_modal_5.showModal();
-        modalSpinner(false);
-  
-    });
+            my_modal_5.showModal();
+            modalSpinner(false);
+
+        });
 }
 
 // display lesson word
-const displayWord = (word)=>{
+const displayWord = (word) => {
     const container = document.getElementById('word-container');
     container.innerHTML = '';
 
-    if(word.length === 0){
+    if (word.length === 0) {
         container.innerHTML = `
    
         <div class="py-8 space-y-4 text-center col-span-full">
@@ -133,7 +162,7 @@ const displayWord = (word)=>{
         `;
     }
 
-    for(let w of word){
+    for (let w of word) {
         const newelement = document.createElement('div');
 
         newelement.innerHTML = `
@@ -152,10 +181,10 @@ const displayWord = (word)=>{
 }
 
 // display lesson label
-const label =(data)=>{
+const label = (data) => {
     const container = document.getElementById('lesson');
     container.innerHTML = '';
-    for(let lesson of data){
+    for (let lesson of data) {
         container.innerHTML += `
         <button id="lesson-${lesson.level_no}" onclick="selectLesson(${lesson.level_no})" class="btn btn-outline btn-primary"><img src="assets/fa-book-open.png"
                                         alt=""> Lesson - ${lesson.level_no}</button>
@@ -164,22 +193,22 @@ const label =(data)=>{
 }
 // FAQ toggle
 function toggleFAQ(button) {
-      const item = button.parentElement;
-      const answer = item.querySelector('.faq-answer');
-      const icon = item.querySelector('.icon');
+    const item = button.parentElement;
+    const answer = item.querySelector('.faq-answer');
+    const icon = item.querySelector('.icon');
 
-      // close all others
-      document.querySelectorAll('.faq-answer').forEach(el => {
+    // close all others
+    document.querySelectorAll('.faq-answer').forEach(el => {
         if (el !== answer) el.classList.add('hidden');
-      });
-      document.querySelectorAll('.faq-question .icon').forEach(el => {
+    });
+    document.querySelectorAll('.faq-question .icon').forEach(el => {
         if (el !== icon) el.textContent = '+';
-      });
+    });
 
-      // toggle clicked one
-      answer.classList.toggle('hidden');
-      icon.textContent = answer.classList.contains('hidden') ? '+' : '-';
-    }
+    // toggle clicked one
+    answer.classList.toggle('hidden');
+    icon.textContent = answer.classList.contains('hidden') ? '+' : '-';
+}
 
 // initial call
 loadData();
